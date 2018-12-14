c9c1bfecdd2ec39b20d5bf1ffebb9b273d8d709a
hideto kashiyama <kwes@nifty.com>
2018-12-14 06:17:39 +0000

    181214-3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411061937) do
+ActiveRecord::Schema.define(version: 20181214055029) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "pnam"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
